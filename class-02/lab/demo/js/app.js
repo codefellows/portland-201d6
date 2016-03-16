60441331a22f1f34e737eb1d02f24d8ccb7115db
Duncan Marsh <dwolfm@gmail.com>
2016-03-16 10:21:21 -0700

    Update app.js

--- class-02/lab/demo/js/app.js
+++ class-02/lab/demo/js/app.js
@@ -3,9 +3,9 @@ var userName = prompt('What is your name?');
 console.log('The user said there name is ' + userName);
 
 alert('Hey there, ' + userName + ' I\'m going to ask you some questions about me. Please answer Y or N.');
+
 var userResponse = prompt('Was, I born on the moon?');
 console.log('userResponse: ' + userResponse);
-
 if (userResponse.toLowerCase() === 'n' || userResponse.toUpperCase() === 'NO') {
     alert('Nope, I was actualy born on the moon. I\'ma moonian computer language.');
 } else if (userResponse.toUpperCase() === 'Y' || userResponse.toUpperCase() === 'YES'){
